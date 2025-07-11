2db758312a6380262281314a6e575a1fcaf5eb76
michael86 <michael8t6@gmail.com>
2025-07-11 21:27:16 +0100

    begun adding nav links

--- src/components/Navbar/NavAside.tsx
+++ src/components/Navbar/NavAside.tsx
@@ -1,3 +1,5 @@
+import { Link } from "@tanstack/react-router";
+
 type Props = {
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -43,11 +45,11 @@ const NavAside: React.FC<Props> = ({ open, setOpen }) => {
         </button>
 
         {/* Insert navigation links here */}
-        <ul className="space-y-2 font-medium">
+        <ul className="space-y-2 font-medium text-white">
           <li>
-            <a
-              href="#"
+            <Link
               className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              to="/dashboard"
             >
               <svg
                 className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
@@ -60,7 +62,73 @@ const NavAside: React.FC<Props> = ({ open, setOpen }) => {
                 <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
               </svg>
               <span className="ms-3">Dashboard</span>
-            </a>
+            </Link>
+          </li>
+          <hr className="border-blue-500" />
+          <li>
+            Invoices
+            <ul>
+              <li>
+                <Link
+                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                  to="/invoices/generate"
+                >
+                  <svg
+                    className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 21"
+                  >
+                    <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                    <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                  </svg>
+                  <span className="ms-3">Generate</span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                  to="/invoices/generate"
+                >
+                  <svg
+                    className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 21"
+                  >
+                    <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                    <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                  </svg>
+                  <span className="ms-3">View</span>
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <hr className="border-blue-500" />
+          <li>
+            Users
+            <ul>
+              <li>
+                <Link
+                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                  to="/invoices/generate"
+                >
+                  <svg
+                    className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 21"
+                  >
+                    <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                    <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                  </svg>
+                  <span className="ms-3">Manage</span>
+                </Link>
+              </li>
+            </ul>
           </li>
         </ul>
       </div>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,15 +3,20 @@ import { useAppSelector } from "../store";
 
 const Dashboard = () => {
   const emailVerified = useAppSelector((s) => s.user.emailVerified);
+  const name = useAppSelector((s) => s.user.firstName);
 
   return (
     <>
       <h1 className="text-center mt-5 text-4xl text-white underline">Dashboard</h1>
       {emailVerified === 0 && (
-        <p>
+        <p className="text-center text-white">
           Please verify your email. If you can't find the email and wish to request another, you can
           do so <Link to="/resend-verification">here</Link>
         </p>
+      )}
+
+      {emailVerified === 1 && (
+        <p className="text-center text-white text-2xl mt-3">Welcome back {name}</p>
       )}
     </>
   );
